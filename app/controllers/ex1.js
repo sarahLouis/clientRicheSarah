4b1570b0b86c1512cb29c890fbce55d1a1ab990b
Sarah <21901307@etu.unicaen.fr>
2021-01-26 21:19:45 +0100

    get style, div note modifiée avec changement de couleurs, fonction update

--- app/controllers/ex1.js
+++ app/controllers/ex1.js
@@ -4,19 +4,31 @@ import { action } from '@ember/object';
 
 export default class Ex1Controller extends Controller {
   @tracked content = 'valeur par defaut';
+  @tracked info = '';
   MAX = 100;
+  lala = '';
+
+  get style(){
+    if (this.size <= 50 && this.size > 20){
+      return 'warning';
+    }
+    if (this.size <= 20) {
+      return 'danger';
+    }
+    return 'info';
+  }
 
   get size() {
     return this.MAX - this.content.length;
   }
   @action save() {
-    var lala = this.content;
-    console.log('test save ' + this.content);
+    this.lala = this.content;
   }
   @action clear(){
     this.content = '';
   }
   @action update(){
-    
+    this.info = 'Note modifiée';
   }
+
 }
--- app/routes/ex1.js
+++ app/routes/ex1.js
@@ -1,4 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default class Ex1Route extends Route {
+  //resuperer les notes dans un tableau et split
+  //le transferer vers exo1b
 }
