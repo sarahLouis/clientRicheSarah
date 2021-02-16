39755e5af6fcbf7aac35420e7c31ee1a970cb929
Sarah <21901307@etu.unicaen.fr>
2021-02-16 16:35:30 +0100

    avancement fonctionnel

--- app/routes/test-new.js
+++ app/routes/test-new.js
@@ -1,9 +1,10 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class TestNewRoute extends Route {
   model(){
     return this.store.createRecord('contact', {
-      nom = 'Smith'
+      nom: 'Smith',
     });
   }
   @action save(contact){
