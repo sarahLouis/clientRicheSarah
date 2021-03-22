1ec14c912691c99c280ca389d1679c7107c3e545
Sarah <21901307@etu.unicaen.fr>
2021-03-22 21:31:25 +0100

    ajout des dernier models + board crée

--- app/models/order.js
+++ app/models/order.js
@@ -1,13 +1,9 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class OrderModel extends Model {
-  @attr('') dateCreation;
-  @attr('') status;
-  @attr('number') amount;
-  @attr('number') toPay;
-  @attr('number') itemsNumber;
-  @attr('number') missingNumber;
-  @attr('number') employee;
-  @attr('number') timeslot;
-  @attr('number') user;
+  @attr('date') dateCreation;
+  @hasMany('orderdetail') orderdetails;
+  get count() {
+    return this.orderdetails.length;
+  }
 }
--- app/routes/board.js
+++ app/routes/board.js
@@ -1,4 +1,17 @@
-import Route from '@ember/routing/route';
+import Abstractroute from './Abstractroute';
+import RSVP from 'rsvp';
 
-export default class BoardRoute extends Route {
+export default class BoardRoute extends Abstractroute {
+  model() {
+    let user = this.userAuth.user;
+    if (user) {
+      return RSVP.hash({
+        orders: this.store.query('order', {
+          filter: { idEmployee: user.id },
+          include: 'orderdetails',
+        }),
+        employee: user,
+      });
+    }
+  }
 }
